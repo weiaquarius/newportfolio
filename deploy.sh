af83f681532c0d68a611b82003a2ab76b6141edf
weiaquarius <weiaquarius17@gmail.com>
2021-04-15 01:39:14 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,13 @@ set -e
 npm run build
 # navigate into the build output directory
 cd dist
+
 cp index.html 404.html
-cd ..
-gh-pages -d dist
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
 git add -A
-git commit -m 'update portfolio'
+git commit -m 'update'
 git push -f git@github.com:weiaquarius/newportfolio.git master:gh-pages
 cd -
